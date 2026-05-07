7d8487e2395e5e145c7de546bc27b45820266d99
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:04:34 +0545

    feat:add conditionals

---
+++ Javascript/3conditional.mjs
@@ -0,0 +1,32 @@
+// if else condition
+
+/* let age =10;
+
+if (age>=18)
+{
+    console.log("You are eligible to vote");
+}
+else
+{
+    console.log("You are not eligible to vote");
+} */
+
+// if else if condition 
+
+let gender ="male";
+
+if (gender==="male")
+{
+    console.log("he");
+}
+else if (gender ==="female ")
+{
+    console.log("she");
+
+
+}
+else{
+    console.log("they");
+}
+
+
