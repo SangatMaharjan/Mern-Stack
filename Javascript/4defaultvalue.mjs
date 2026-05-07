a4de0a3e0b94698adb39cf1408f6d7b55bc9398a
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:04:58 +0545

    feat:add default values

---
+++ Javascript/4defaultvalue.mjs
@@ -0,0 +1,17 @@
+// Default Value 
+
+let info = (name , surname , weight)=> 
+{
+    console.log(name , surname , weight)
+
+}
+info("Lebron")
+
+let info1=(name = "Steph ", surname ="Curry ", weight =80)=>
+{
+    console.log(name , surname , weight)
+}
+
+info1()
+info1("Devin " , "Booker ", 65)
+
