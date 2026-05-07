41f1eee20ac7f5bba9224ec75f2a3364527def1a
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:10:21 +0545

    feat:exporting the file

---
+++ Javascript/export.mjs
@@ -0,0 +1,9 @@
+export let variable = "Hello"
+console.log(variable)
+
+let country="nepal"
+export default country
+
+/* export by name : prefix by export 
+we can have multiple export by name in a file
+export by default : we have only one export by default on a file  */
