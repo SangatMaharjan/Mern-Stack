7e0e4ff924ab0f0370d9d0e1827dcdee52da6649
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:08:24 +0545

    feat:add object concept

---
+++ Javascript/8object.mjs
@@ -0,0 +1,14 @@
+let info1=
+{
+    name: "nicolas",
+    weight:65,
+    isMarried : false
+};
+console.log(info1)
+console.log(info1.weight)
+
+info1.weight=70
+console.log(info1.weight)
+
+info1.isMarried=true
+console.log(info1.isMarried)
