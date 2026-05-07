3b063efb24de82a117edd83d086da0294ab80f35
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:03:50 +0545

    feat:add operator basics

---
+++ Javascript/1operator.mjs
@@ -0,0 +1,31 @@
+//Arithmetic operator 
+console.log(1+1)
+console.log(2-1)
+console.log(2*3)
+console.log(10/2)
+console.log(10%3)
+
+// Assignment operator 
+
+let a = 10 
+
+//comparison operator 
+console.log(3>2)
+
+console.log (3>=3)
+console.log(3<2)
+console.log(3===3) //More Strct Comparison 
+
+console.log(3=="3") //Loose comparison tries to convert data type 
+console.log(3==="3") //Strict Comparison
+
+// Logial Operator 
+
+console.log(true && true)
+console.log(true && false)  
+
+console.log(true || false)
+
+console.log(false || true || false) //Answer : True ( One must be True )
+
+console.log(!false && true )
