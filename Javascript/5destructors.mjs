5bc29336a2fd76d40cdf993a17f089c67d9489da
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:07:05 +0545

    feat:add destructors

---
+++ Javascript/5destructors.mjs
@@ -0,0 +1,24 @@
+//defining destructors 
+ 
+let [a,b,c]=[10,11,12];
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+let [d,e,f]=["nicolas" , "delap" , "garnacho"]
+
+console.log(d)
+console.log(e)
+console.log(f)      
+
+//object destructors 
+
+let {isGay , weight , name }={name: "sworup" , weight : 100 , isGay : true}
+
+console.log(name)
+console.log(weight)
+console.log(isGay)
+
+// In array order matters but in object it does not matter
+
