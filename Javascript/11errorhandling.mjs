3b2d7aa4764265d5789a19b93a4a6575aac5b1c0
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:09:10 +0545

    feat:add error handling

---
+++ Javascript/11errorhandling.mjs
@@ -0,0 +1,12 @@
+try 
+{
+    console.log(name)
+    conosle.log("this is try block")
+
+}
+catch(error)
+{
+    console.log("this is catch block")
+    console.log("Error:" + error)
+    console.log(error.message)
+}
