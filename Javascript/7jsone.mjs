487200df33322f287110897b0eac2da5760a267b
Sangat Maharjan <sangat062@gmail.com>
2026-05-07 16:08:00 +0545

    feat:add jsone

---
+++ Javascript/7jsone.mjs
@@ -0,0 +1,39 @@
+console.log("Hello World")
+console.log(3)
+// console.log(true)
+console.log(true)
+
+console.log(true) 
+
+console.log(true)
+console.log(true)
+console.log(true)
+console.log(true)
+console.log(true)
+/* console.log(true)
+console.log(true)
+console.log(true)
+console.log(true) */
+/* 
+number 1 , 2, 3, 3.2
+string "a" "lebron" "1 " "true "
+boolean true false 
+ */
+
+//variables
+let name = "lebron"
+let ward =1
+let address = "los angeles "
+let hascar = true
+
+//calling variables 
+console.log(name,ward , address , hascar)
+
+//changing variables
+
+name ="michael "
+console.log(name)
+
+const name1="stephen "
+console.log(name1)
+ 
