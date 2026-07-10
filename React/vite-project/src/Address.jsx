4a6b789f0e61f2087fbb1a69c248ecc7a0b91e42
Sangat Maharjan <sangat062@gmail.com>
2026-07-10 21:24:05 +0545

    feat:creating components

---
+++ React/vite-project/src/Address.jsx
@@ -0,0 +1,22 @@
+// Creating Components 
+
+const Address = ({country , city , province})=>
+    {
+    return<div>
+        <h1>Portugal</h1>
+        <p>Bad Boys Bad Boys </p>
+        <p>Country is {country} City is {city} and province is {province}</p>
+        </div>
+   
+    
+
+}
+
+
+
+export default Address;
+
+// make tag with name Address
+// We need to use the Component in App.jsx file 
+
+
