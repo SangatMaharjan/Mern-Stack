bb4840d4da101e4b72c02c6a36d32e2520534f74
Sangat Maharjan <sangat062@gmail.com>
2026-07-13 20:42:04 +0545

    feat:add changes made to react

---
+++ React/vite-project/src/Father.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Father = ({name,surname,age,profession}) => {
+  return (
+    <div>
+       Galaxy Destroyer 
+       <h1>Father Attributes</h1>
+       <p>Name : {name}</p>
+       <p>Surname: {surname}</p>
+       <p>Age : {age}</p>
+       <p>Profession : {profession}</p>
+      
+    </div>
+  )
+}
+
+export default Father
+
+// Inorder to create a component we can use rafce snippet
+
