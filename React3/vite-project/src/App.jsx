dd7b4b568f7a83847d600f6dce8b6a928afc0431
Sangat Maharjan <sangat062@gmail.com>
2026-07-17 21:11:33 +0545

    feat:add defining navlink

---
+++ React3/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CosmosNavlink from './component/CosmosNavlink'
+import CosmosRoute from './component/CosmosRoute'
+
+const App = () => {
+  return (
+    <div>
+      <CosmosNavlink></CosmosNavlink>
+      <CosmosRoute></CosmosRoute>
+    </div>
+  )
+}
+
+export default App
